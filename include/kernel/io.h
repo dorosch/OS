7bdaa7c03cb56a362771fd47997a94fb9d816ce9
Doroschenko Andrey <andrei.tifon@yandex.ru>
2017-02-13 17:04:52 +0300

    Rename function 'outb -> write_port' and 'inb -> read_port' in 'include/kernel/io.h'

--- include/kernel/io.h
+++ include/kernel/io.h
@@ -4,7 +4,7 @@
 #include "stdint.h"
 
 
-void outb(uint16_t , uint8_t );
-uint8_t inb(uint16_t );
+void write_port(uint16_t , uint8_t );
+uint8_t read_port(uint16_t );
 
 #endif
--- kernel/io.c
+++ kernel/io.c
@@ -6,7 +6,7 @@
 /**
  * Writing int the I/O port values.
  */
-void outb(uint16_t port, uint8_t value) {
+void write_port(uint16_t port, uint8_t value) {
     __asm__ __volatile__ (
         "outb %1, %0"
         :
@@ -20,7 +20,7 @@ void outb(uint16_t port, uint8_t value) {
 /**
  * Reading the value of the I/O port.
  */
-uint8_t inb(uint16_t port) {
+uint8_t read_port(uint16_t port) {
     uint8_t result;
 
     __asm__ __volatile__ (
